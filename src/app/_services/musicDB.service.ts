c5acc428b708ed49e3f8df4df94fee5404a5052e
ZioCain <micael.castelleits@gmail.com>
2023-09-08 10:39:33 +0200

    Tests for filesystem

--- src/app/_services/musicDB.service.ts
+++ src/app/_services/musicDB.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import localforage from "localforage";
-import { Filesystem, Directory, ReaddirResult, PermissionStatus } from '@capacitor/filesystem';
+import { Filesystem, Directory, ReaddirResult, PermissionStatus, FileInfo } from '@capacitor/filesystem';
+import { Song } from '../_models/song';
 
 const MUSIC_DB_KEY:string = 'db';
 
@@ -16,16 +17,26 @@ export class MusicDBService {
 	}
 
 	public ScanDirectories(){
-		Filesystem.requestPermissions().then((status:PermissionStatus)=>{
-			console.log("PERM: "+JSON.stringify(status, null, 4));
-			return status;
-		});
-
 		Filesystem.readdir({
-			path: '/',
-			directory: Directory.ExternalStorage
+			path: '/sdcard/',
+			//directory: Directory.ExternalStorage
 		}).then((res:ReaddirResult)=>{
 			console.log("FILESLIST: "+JSON.stringify(res.files, null, 4));
+			res.files
+				.filter((x:FileInfo)=>x.type === "file")
+				.forEach((file:FileInfo)=>{
+					window.jsmediatags.read(file.uri,{
+						onSuccess: (info:any)=>{
+							const song = new Song({
+								...info.tags,
+								dateAdded: new Date(),
+								plays: 0,
+								file: file.uri
+							});
+						},
+						onError: (err:any)=>{}
+					})
+				});
 		}).catch((err:any)=>{
 			console.error("FILELISTERR "+JSON.stringify(err, null, 4));
 		})
